8eb3a3d9f929fbd0bb56297b61d955162b82d767
trmoro <56671926+trmoro@users.noreply.github.com>
2025-05-20 22:21:14 +0200

    CH-134 revert db

--- mongo.py
+++ mongo.py
@@ -4,9 +4,9 @@ import pymongo
 #Get Document Db
 def get_mongo_db():
 	client = pymongo.MongoClient(os.environ["MONGOKEY"], connect=False)
-	return client, client["v1"]
+	return client, client["cnvhub"]
 
 #Get Document Db
 def get_mongo_db2():
 	client = pymongo.MongoClient(os.environ["MONGOKEY2"], connect=False)
-	return client, client["cnvhub"]
+	return client, client["v1"]
